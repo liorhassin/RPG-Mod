5c1fa03ffc18108e34a4aac60d069d466c632aa9
Nadav <nadavp132@gmail.com>
2022-09-13 21:58:14 +0300

    Portal

---
+++ src/main/java/net/LiorNadav/rpgmod/villager/ModPOIs.java
@@ -0,0 +1,25 @@
+package net.LiorNadav.rpgmod.villager;
+
+import com.google.common.collect.ImmutableSet;
+import net.LiorNadav.rpgmod.RPGMod;
+import net.LiorNadav.rpgmod.block.ModBlocks;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModPOIs {
+    public static final DeferredRegister<PoiType> POI
+            = DeferredRegister.create(ForgeRegistries.POI_TYPES, RPGMod.MOD_ID);
+
+    public static final RegistryObject<PoiType> KAUPEN_PORTAL =
+            POI.register("kaupen_portal", () -> new PoiType(
+                    ImmutableSet.copyOf(ModBlocks.KAUPEN_PORTAL.get().getStateDefinition().getPossibleStates())
+                    , 0, 1));
+
+
+    public static void register(IEventBus eventBus) {
+        POI.register(eventBus);
+    }
+}
---
+++ src/main/java/net/LiorNadav/rpgmod/world/dimension/ModDimensions.java
@@ -0,0 +1,20 @@
+package net.LiorNadav.rpgmod.world.dimension;
+
+import net.LiorNadav.rpgmod.RPGMod;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
+
+public class ModDimensions {
+    public static final ResourceKey<Level> KJDIM_KEY = ResourceKey.create(Registry.DIMENSION_REGISTRY,
+            new ResourceLocation(RPGMod.MOD_ID,"kjdim"));
+
+    public static final ResourceKey<DimensionType> KJDIM_TYPE =
+            ResourceKey.create(Registry.DIMENSION_TYPE_REGISTRY, KJDIM_KEY.registry());
+
+    public static void register(){
+        System.out.println("Registering ModDimension for " + RPGMod.MOD_ID);
+    }
+}
