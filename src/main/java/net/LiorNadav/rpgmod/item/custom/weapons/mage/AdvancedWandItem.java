c405685a72a399919b759d90c51bc48390f9e41f
Lior Hassin <liorhassin3@gmail.com>
2022-09-20 16:11:49 +0300

    fireball customized, all tier wands added

---
+++ src/main/java/net/LiorNadav/rpgmod/item/custom/weapons/mage/AdvancedWandItem.java
@@ -0,0 +1,21 @@
+package net.LiorNadav.rpgmod.item.custom.weapons.mage;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class AdvancedWandItem extends Item {
+    public AdvancedWandItem(Properties pProperties) {
+        super(pProperties);
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if(hand == InteractionHand.MAIN_HAND) {
+            MageSkills.fireball(player.getMainHandItem(), level, player, 4, false, false, 5, 16);
+        }
+        return super.use(level, player, hand);
+    }
+}
