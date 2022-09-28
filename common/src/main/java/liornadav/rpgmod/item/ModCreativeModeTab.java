49b4ecf816041f9f0e613d1820d9c34a776eb50d
Lior Hassin <liorhassin3@gmail.com>
2022-09-28 15:05:27 +0300

    more fixes

--- common/src/main/java/liornadav/rpgmod/item/ModCreativeModeTab.java
+++ common/src/main/java/liornadav/rpgmod/item/ModCreativeModeTab.java
@@ -1,16 +1,17 @@
 package liornadav.rpgmod.item;
 
+import dev.architectury.registry.CreativeTabRegistry;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab RPG_MOD_WEAPONS = new CreativeModeTab("rpgmod_weapons_tab") {
+    public static final CreativeTabRegistry RPG_MOD_WEAPONS = new CreativeTabRegistry("rpgmod_weapons_tab") {
         @Override
-        public ItemStack makeIcon() {
+        public ItemGroup makeIcon() {
             return new ItemStack(ModItems.BEGINNER_BROADSWORD.get());
         }
     };
-    public static final CreativeModeTab RPG_MOD_ITEMS = new CreativeModeTab("rpgmod_items_tab") {
+    public static final CreativeTabRegistry RPG_MOD_ITEMS = new CreativeTabRegistry("rpgmod_items_tab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.ADAMANTITE_INGOT.get());
